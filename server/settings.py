f0001f836c27b8ed60019a43b4cf9575d1e104c0
Vaishnav Murali <vaishnavm217>
2017-08-30 12:47:03 +0530

    making it ready for heroku

--- server/settings.py
+++ server/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '7op^nso$m4jrh99dqy$$8s$5ucl_jem1a=64$!u8gl!22%-z-e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+db_from_env = dj_database_url.config(conn_max_age=500)
 ALLOWED_HOSTS = []
 
 
@@ -46,7 +46,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
-}	
+}			
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
