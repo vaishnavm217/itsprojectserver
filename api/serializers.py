a41c06739b8daa2296385589061e53f6012ef491
Vaishnav Murali <vaishnavm217>
2017-09-12 22:28:29 +0530

    Me

--- api/serializers.py
+++ api/serializers.py
@@ -2,42 +2,42 @@ from api.models import Houses, Members, Photos, Videos, Farms, Crops, Wells, Wel
 from rest_framework import serializers
 
 class HousesSerializer(serializers.ModelSerializer):
-	class Meta:
-        	model = Houses
-        	fields = ('HID', 'Long', 'Lat', 'Income')
+    class Meta:
+        model = Houses
+        fields = ('HID', 'point', 'Income')
 
 
 class MembersSerializer(serializers.ModelSerializer):
-    	class Meta:
-        	model = Members
-        	fields = ('HID','Name', 'PID', 'Age', 'Gender')
+    class Meta:
+        model = Members
+        fields = ('HID','Name', 'PID', 'Age', 'Gender')
 
 class PhotosSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Photos
-		fields = ('HID','PHID','URL')
+    class Meta:
+        model = Photos
+        fields = ('HID','PHID','URL')
 
 class VideosSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Videos
-		fields = ('HID','VID','URL')
+    class Meta:
+        model = Videos
+        fields = ('HID','VID','URL')
 
 class FarmsSerializer(serializers.ModelSerializer):
-        class Meta:
-                model = Farms
-                fields = ('HID', 'FID', 'Long', 'Lat')
+    class Meta:
+        model = Farms
+        fields = ('HID', 'FID', 'plot')
 
 class CropsSerializer(serializers.ModelSerializer):
-        class Meta:
-                model = Crops
-                fields = ('FID', 'Year', 'Seasons')
+    class Meta:
+        model = Crops
+        fields = ('FID', 'Year', 'Seasons')
 
 class WellsSerializer(serializers.ModelSerializer):
-        class Meta:
-                model = Wells
-                fields = ('HID', 'WID','Long', 'Lat', 'AvgYield' )
+    class Meta:
+        model = Wells
+        fields = ('HID', 'WID','point', 'AvgYield' )
 
 class WellWaterSerializer(serializers.ModelSerializer):
-        class Meta:
-                model = WellWater
-                fields = ('WID', 'Year', 'measured_date')
+    class Meta:
+        model = WellWater
+        fields = ('WID', 'Year', 'measured_date')
