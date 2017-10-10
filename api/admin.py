8b88f84460167ae457675437f0f0f324a8a993f4
Vaishnav Murali <vaishnavmurali@gmail.com>
2017-10-11 03:44:35 +0530

    video,audio and img fix

--- api/admin.py
+++ api/admin.py
@@ -6,12 +6,20 @@ class ImageAdmin(admin.OSMGeoAdmin):
     # explicitly reference fields to be shown, note image_tag is read-only
     fields = ( 'Type','HID','FID','WID','image_tag','photo' )
     readonly_fields = ('image_tag',)
+class VideoAdmin(admin.OSMGeoAdmin):
+    # explicitly reference fields to be shown, note image_tag is read-only
+    fields = ( 'Type','HID','FID','WID','image_tag','video' )
+    readonly_fields = ('image_tag',)
+class AudioAdmin(admin.OSMGeoAdmin):
+    # explicitly reference fields to be shown, note image_tag is read-only
+    fields = ( 'Type','HID','FID','WID','image_tag','audio' )
+    readonly_fields = ('image_tag',)
 admin.site.register(Houses,admin.OSMGeoAdmin)
 admin.site.register(Members,admin.OSMGeoAdmin)
 admin.site.register(Farms,admin.OSMGeoAdmin)
 admin.site.register(Photos,ImageAdmin)
-admin.site.register(Videos,admin.OSMGeoAdmin)
-admin.site.register(Audios,admin.OSMGeoAdmin)
+admin.site.register(Videos,VideoAdmin)
+admin.site.register(Audios,AudioAdmin)
 admin.site.register(Crops,admin.OSMGeoAdmin)
 admin.site.register(Wells,admin.OSMGeoAdmin)
 admin.site.register(Yields,admin.OSMGeoAdmin)
