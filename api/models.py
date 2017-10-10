3f02851f9ec4ab981cf3c17b1352644b2ec9f371
savitha97 <savithag1197@gmail.com>
2017-10-10 20:05:28 +0530

    db

--- api/models.py
+++ api/models.py
@@ -43,10 +43,10 @@ class Crops(models.Model):
         return "%s : %s" %(self.FID,self.Year)
 
 class Wells(models.Model):
-    HID=models.ForeignKey(Houses,to_field='HID',on_delete=models.CASCADE)
+    FID=models.ForeignKey(Farms,to_field='FID',on_delete=models.CASCADE)
     WID=models.AutoField(primary_key=True)
     point=models.PointField(default=Point(1,1))
-    AvgYield=models.DecimalField(max_digits=7,decimal_places=4)
+    AvgYield=models.DecimalField(max_digits=7,decimal_places=4,default=0)
     Depth=models.DecimalField(max_digits=7,decimal_places=4)
     def __str__(self):
         return "%s" %(self.WID)
@@ -54,11 +54,16 @@ class Wells(models.Model):
 class Yields(models.Model):
     WID=models.ForeignKey(Wells,to_field='WID',on_delete=models.CASCADE)
     Yield=models.FloatField(default=0.0)
-    measured_date=models.DateField(default=datetime.date.today)
+    measured_date=models.DateTimeField(default=datetime.datetime.now())
     def __str__(self):
-        return "%s : %s" %(self.WID,self.WID)
-
-
+        return "%s : %s" %(self.WID,self.measured_date)
+    def save(self):
+	super().save(self)
+	temp = Yields.objects.filter(WID=self.WID)
+	avg = sum([i.Yield for i in temp])/len(temp)
+	wellt = Wells.object.filter(WID=self.WID)
+	wellt.AvgYield=avg
+	wellt.save()
 
 class Photos(models.Model):
     types=(('WID','WID'),('FID','FID'),('HID','HID'))
@@ -67,7 +72,7 @@ class Photos(models.Model):
     FID=models.ForeignKey(Farms,to_field='FID',on_delete=models.CASCADE,blank=True)
     WID=models.ForeignKey(Wells,to_field='WID',on_delete=models.CASCADE,blank=True)
     PHID=models.AutoField(primary_key=True)
-    file=models.FileField(upload_to = 'uploaded_images/')
+    file=models.ImageField(upload_to = 'uploaded_images/')
     def __str__(self):
         return "%s : %s" % (self.HID,self.PHID)
     @property
@@ -85,9 +90,8 @@ class Videos(models.Model):
     HID=models.ForeignKey(Houses,to_field='HID',on_delete=models.CASCADE,blank=True)
     FID=models.ForeignKey(Farms,to_field='FID',on_delete=models.CASCADE,blank=True)
     WID=models.ForeignKey(Wells,to_field='WID',on_delete=models.CASCADE,blank=True)
-
     VID=models.AutoField(primary_key=True)
-    file=models.ImageField(upload_to = 'uploaded_video/')
+    file=models.FileField(upload_to = 'uploaded_video/')
     def __str__(self):
         return "%s : %s" % (self.HID,self.VID)
     @property
